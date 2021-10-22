977ca22e765783bd4e02d4f476045e73f3a06df1
Tarxos <ftarxos@gmail.com>
2021-10-22 19:54:10 +0300

    add views

---
+++ Views/Configurations/SizePanelConfiguration.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Form.Views.Configurations
+{
+    public partial class SizePanelConfiguration : Page
+    {
+        public SizePanelConfiguration() => InitializeComponent();
+    }
+}
