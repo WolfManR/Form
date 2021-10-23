39d1972d4a611fa43242dd4fd9b115b0568cf04e
Tarxos <ftarxos@gmail.com>
2021-10-23 14:15:12 +0300

    change app initialization

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,22 @@
 ﻿using System.Windows;
+using Form.ViewModels;
+using Form.Views;
 
 namespace Form
 {
     public partial class App : Application
     {
+        private static readonly MainVM MainVM = new();
         public App()
         {
+            var mainWindow = new MainWindow()
+            {
+                DataContext = MainVM,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            };
 
+            MainWindow = mainWindow;
+            mainWindow.Show();
         }
     }
 }
