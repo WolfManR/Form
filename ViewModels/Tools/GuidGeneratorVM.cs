8b3fdcc458a06295b6a13919cd573f384e8f6da0
Tarxos <ftarxos@gmail.com>
2021-10-23 17:49:18 +0300

    add prototype of Guid Generator

--- ViewModels/Tools/GuidGeneratorVM.cs
+++ ViewModels/Tools/GuidGeneratorVM.cs
@@ -1,9 +1,41 @@
-﻿using Thundire.MVVM.WPF.Observable.Base;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Thundire.MVVM.WPF.Commands.Relay;
+using Thundire.MVVM.WPF.Observable.Base;
 
 namespace Form.ViewModels.Tools
 {
     public class GuidGeneratorVM : NotifyBase
     {
-        
+        private string _generatedGuid;
+        private bool _isAutoCopyToClipboard;
+        private bool _isGenerateGuidInUpperCase;
+
+        public GuidGeneratorVM()
+        {
+            CopyGeneratedGuidToClipboardCommand = new RelayCommand(CopyGeneratedGuidToClipboard, () => !GeneratedGuid.IsNotFilled());
+            GenerateCommand = new RelayCommand(Generate);
+        }
+
+        public string GeneratedGuid { get => _generatedGuid; set => Set(ref _generatedGuid, value); }
+        public bool IsAutoCopyToClipboard { get => _isAutoCopyToClipboard; set => Set(ref _isAutoCopyToClipboard, value); }
+        public bool IsGenerateGuidInUpperCase { get => _isGenerateGuidInUpperCase; set => Set(ref _isGenerateGuidInUpperCase, value); }
+
+        public ICommand CopyGeneratedGuidToClipboardCommand { get; }
+        public ICommand GenerateCommand { get; }
+
+        private void CopyGeneratedGuidToClipboard()
+        {
+            Clipboard.SetText(GeneratedGuid);
+        }
+
+        private void Generate()
+        {
+            var guid = Guid.NewGuid().ToString();
+            if(IsGenerateGuidInUpperCase) guid = guid.ToUpper();
+            GeneratedGuid = guid;
+            if(_isAutoCopyToClipboard) CopyGeneratedGuidToClipboard();
+        }
     }
 }
