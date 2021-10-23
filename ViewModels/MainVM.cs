2af50265ee4b5a04e549cd73cbea7e77be003bc3
Tarxos <ftarxos@gmail.com>
2021-10-23 14:18:53 +0300

    add views to MainWindow

--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -1,9 +1,17 @@
-﻿using Thundire.MVVM.WPF.Observable.Base;
+﻿using Form.ViewModels.Tools;
+using Thundire.MVVM.WPF.Observable.Base;
 
 namespace Form.ViewModels
 {
     public class MainVM : NotifyBase
     {
-        
+        public MainVM()
+        {
+            SizePanelVM = new();
+            IconPaintVM = new();
+        }
+
+        public SizePanelVM SizePanelVM { get; }
+        public IconPaintVM IconPaintVM { get; }
     }
 }
